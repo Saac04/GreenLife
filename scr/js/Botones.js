606bf343552076bfb1de83c9b16a5f9c6b5535b0
Saac04 <santiagoaaguirrec@gmail.com>
2024-05-06 19:45:27 +0200

    Css botones, fuentes y otros

---
+++ scr/js/Botones.js
@@ -0,0 +1,12 @@
+function toggleActive(button) {
+    button.classList.toggle("active");
+}
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
