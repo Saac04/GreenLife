5050473c60575a6534752c59170b1fd98533f893
ALANGMupv <alanguevara36@gmail.com>
2024-05-07 16:49:15 +0200

    solo falta marcos y paginas a las que se redirige

--- scr/js/inicioSesion.js
+++ scr/js/inicioSesion.js
@@ -1,13 +1,32 @@
-window.onload = function() {
-    var emailInput = document.querySelector('input[type="email"]');
-    var passwordInput = document.querySelector('input[type="password"]');
-    
-    emailInput.onfocus = function() { //dispara el foco en el emailInput
-        this.placeholder = ''; //cuando está en emailInput, se borra el texto escrito 
-    }
-    
-    passwordInput.onfocus = function() {
-        this.placeholder = '';
-    }
-} //hola
+function iniciarSesion() {
+    var emailInput = document.querySelector('input[type="email"]').value;
+    var passwordInput = document.querySelector('input[type="password"]').value;
 
+    // Definir los usuarios válidos
+    var usuariosValidos = [
+        { correo: "soytecnico@gmail.com", contraseña: "hola1234" },
+        { correo: "soyusuario@gmail.com", contraseña: "hola1234" },
+        { correo: "soyadministrador@gmail.com", contraseña: "hola1234" }
+    ];
+
+    // Verificar si las credenciales coinciden con algún usuario válido
+    var usuarioEncontrado = usuariosValidos.find(function(usuario) {
+        return usuario.correo === emailInput && usuario.contraseña === passwordInput;
+    });
+
+    var mensajeError = document.getElementById('mensaje-error');
+
+    if (usuarioEncontrado) {
+        // Redirigir al usuario a una página en el futuro
+        // Aquí puedes cambiar la URL por la de la página a la que quieras redirigir al usuario
+        window.location.href = "pagina_futura.html";
+    } else {
+        // Credenciales inválidas
+        mensajeError.innerText = "Credenciales inválidas. Por favor, inténtelo de nuevo.";
+    }
+}
+
+function volver() {
+    // Aquí puedes cambiar la URL por la de la página a la que quieras redirigir al usuario
+    window.location.href = "pagina_volver.html";
+}
